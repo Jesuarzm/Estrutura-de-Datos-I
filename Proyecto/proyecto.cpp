c012731498161ae794cd2f23d8cb531216f9091a
Jesuarzm <jesuarzambrano@hotmail.com>
2023-03-04 20:26:18 -0600

    Terminar puntos 5.7-5.8-5.9

--- Proyecto/proyecto.cpp
+++ Proyecto/proyecto.cpp
@@ -133,9 +133,102 @@ void mostrarLista(lista cabeza){
 		
 	}
 };
+void contarContatos(lista cabeza){
+	lista aux;
+	nodoUsuario *auxUsr;
+	int cantUsr = 0;
+	if(cabeza ==NULL){
+		cout << "Sin contactos registrados." << endl;
+	}else{
+		aux=cabeza;
+		while (aux!=NULL){	
+			if (aux->sigUsuario!=NULL)
+			{
+				auxUsr=aux->sigUsuario;
+				while (auxUsr!=NULL)
+				{
+					cantUsr++;
+					auxUsr=auxUsr->sig;
+				}
+				
+			}
+			aux=aux->sig;
+		}
+		cout << "**********************" << endl;
+		cout << "Cantidad de contactos: " << cantUsr << endl;
+		cout << "**********************" << endl;
+		
+	}
+};
+void contarProvincia(lista cabeza, string prov){
+	lista aux;
+	nodoUsuario *auxUsr;
+	int cantUsr = 0;
+	if(cabeza ==NULL){
+		cout << "Lista Vacia" << endl;
+	}else{
+		aux=cabeza;
+		while (aux!=NULL){	
+			if (aux->sigUsuario!=NULL)
+			{
+				auxUsr=aux->sigUsuario;
+				while (auxUsr!=NULL)
+				{
+					if (auxUsr->provincia == prov)
+					{
+						cantUsr++;
+					}
+					
+					auxUsr=auxUsr->sig;
+				}
+				
+			}
+			aux=aux->sig;
+		}
+		cout << "**********************" << endl;
+		cout << "Provincia: " << prov << endl;
+		cout << "**********************" << endl;
+		cout << "Cantidad de contactos: " << cantUsr << endl;
+		cout << "**********************" << endl;
+	}
+};
+void contarLetras(lista cabeza, char letra){
+	lista aux;
+	nodoUsuario *auxUsr;
+	int cantUsr = 0;
+	if(cabeza ==NULL){
+		cout << "Lista Vacia" << endl;
+	}else{
+		aux=cabeza;
+		while (aux!=NULL){	
+			if (aux->sigUsuario!=NULL)
+			{
+				auxUsr=aux->sigUsuario;
+				while (auxUsr!=NULL)
+				{
+					if (auxUsr->primerApellido[0]== letra)
+					{
+						cantUsr++;
+					}
+					
+					auxUsr=auxUsr->sig;
+				}
+				
+			}
+			aux=aux->sig;
+		}
+		cout << "**********************" << endl;
+		cout << "Letra : " << letra << endl;
+		cout << "**********************" << endl;
+		cout << "Cantidad de contactos: " << cantUsr << endl;
+		cout << "**********************" << endl;
+	}
+};
 int main(){
-	lista Lista=NULL;
 
+	bool estado = true;
+	
+	lista Lista=NULL;
 	ingresarAbc(Lista, 'A');
 	ingresarAbc(Lista, 'B');
 	ingresarAbc(Lista, 'C');
@@ -166,6 +259,66 @@ int main(){
     ingresarUsuario(Lista, "Jesuar", "Miranda", "Zambrano", 84092380,
     84092380 , 84092380, 82558001, "jesuarzambrano@hotmail.com", "CHepe");
 
-    mostrarLista(Lista);
+	
+	
+	do{
+		char letraIn;
+		string prov;
+        int opc = 0;
+		system("clear");
+        cout << "Menu:" << endl;
+        cout << "****** Contactos *****" << endl;
+        cout << "1: Agregar " << endl;
+        cout << "2: Borrar " << endl;
+        cout << "3: Editar " << endl;
+        cout << "****** Busqueda *****" << endl;
+        cout << "4: Por numero" << endl;
+        cout << "5: Por cedula" << endl;
+        cout << "****** Reportes *****" << endl;
+        cout << "6: Por Provincia" << endl;
+        cout << "7: Contactos Registrados" << endl;
+        cout << "8: Cantidad por provincia" << endl;
+        cout << "9: Cantidad por inicial" << endl;
+        cout << "*********************" << endl;
+        cout << "10: SALIR" << endl;
+        
+        cin >> opc;
 
+		if (opc == 7){
+			system("clear");
+			contarContatos(Lista);
+			system("read -rp $'Press [Enter] to continue...\n' key");
+			opc = 0;
+		}
+		if (opc == 8){
+			system("clear");
+			cout << "Ingresa la provincia a contar" << endl;
+			cin >> prov;
+			contarProvincia(Lista, prov);
+			system("read -rp $'Press [Enter] to continue...\n' key");
+			opc = 0;
+		}
+		if (opc == 9){
+			system("clear");
+			cout << "Ingresa la letra a contar" << endl;
+			cin >> letraIn;
+			contarLetras(Lista, letraIn);
+			system("read -rp $'Press [Enter] to continue...\n' key");
+			opc = 0;
+		}
+		
+		if (opc == 10)
+		{
+			estado = false;
+		}/*
+		if (opc != 1 or 2 or 3 or 4 
+		or 5 or 6 or 7 or 8 or 9 or 10){
+			system("clear");
+			cout << "Ingresar una opcion valida:" << endl;
+			opc = 0;
+		}*/
+		
+		
+	}while (estado == true);
+	
 };
